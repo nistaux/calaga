ac3ec5a3b3df2d6820eb0507bbf433010796a13f
Nistaux <nistaux@gmail.com>
2023-08-23 12:18:49 -0500

    added enemies boiler plate for eventually spawning

---
+++ src/enemies.c
@@ -0,0 +1,63 @@
+#include <stdbool.h>
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+#include "enemies.h"
+
+SDL_Texture *enemiesTexture;
+int total_enemies = 0;
+Enemy enemies[POSSIBLE_ENEMIES];
+
+void init_enemies(SDL_Renderer *renderer){
+    enemiesTexture = IMG_LoadTexture(renderer, ENEMY_SPRITE_DIR);
+}
+
+int redistribute_enemy_array(){
+    if(total_enemies == 0){return 0;}
+    int arr_count = 0;
+    for(int i = 0; i < POSSIBLE_ENEMIES; i++){
+        if(enemies[i].created == true){
+            //printf("Set index[%i] from prev index[%i]\n", arr_count, i);
+            enemies[arr_count] = enemies[i];
+            arr_count++;
+        }
+    }
+    for(int i = 0; i < (POSSIBLE_ENEMIES-arr_count); i++) {
+        Enemy proj = {
+            .created = false,
+        };
+        //printf("Zeroing out index[%i]\n", arr_count+i);
+        enemies[arr_count+i] = proj;
+    }
+    return arr_count;
+}
+
+void create_beader(){
+    Enemy enemy;
+    // create the struct w/ props
+
+    int next_element = redistribute_enemy_array();
+    enemies[next_element] = enemy;
+}
+void create_enemy(EnemyType type){
+    switch (type){
+        case ENEMY_TYPE_BEADER:
+            create_beader();
+            break;
+        default:
+            break;
+    }
+}
+
+void move_enemies(){
+
+}
+
+void tick_enemies(){
+    move_enemies();
+}
+
+void draw_enemies(SDL_Renderer *renderer){
+    
+}
---
+++ src/enemies.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include <SDL.h>
+
+#define ENEMY_SPRITE_DIR "./res/images/enemies.png"
+#define POSSIBLE_ENEMIES 10
+
+typedef enum {
+    ENEMY_TYPE_BEADER,
+    ENEMY_TYPE_DAGGER,
+    ENEMY_TYPE_TSHOT,
+    ENEMY_TYPE_ASTEROID
+} EnemyType;
+
+typedef enum {
+    ENEMY_STATE_SHOOTING,
+    ENEMY_STATE_RECHARGING,
+    ENEMY_STATE_DEAD
+} EnemyState;
+
+typedef struct {
+    EnemyType type;
+    EnemyState state;
+    SDL_Rect spriteRect;
+    bool created;
+    float x;
+    float y;
+    int hp;
+    float x_vel;
+    float y_vel;
+    float shoot_reload_interval_seconds;
+    float last_shot;
+    bool reloading;
+} Enemy;
+
+void init_enemies(SDL_Renderer *renderer);
+void create_enemy(EnemyType type);
+void tick_enemies();
+void draw_enemies(SDL_Renderer *renderer);
