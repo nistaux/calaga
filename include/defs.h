a29289cdb5a941f7956c560e92add54d40258b15
nistaux <nistaux@gmail.com>
2023-09-13 10:11:16 -0500

    made changes to versioning

--- include/defs.h
+++ include/defs.h
@@ -2,4 +2,10 @@
 
 #define GAME_WIDTH 600
 #define GAME_HEIGHT 800
-#define GAME_NAME "Calaga v0.6.1 (Alpha)"
+
+/*
+version is vX.X.X.Julian Date since 2021 Jan 25 
+                                   -> (short hash for git commit) 
+                                             -> (Build Version)
+*/
+#define GAME_NAME "Calaga v0.6.1.0956 (6114644) (Alpha x64)"
