3ba42fd956925cbb1c40d0ae0060809892806ea8
nistaux <nistaux@gmail.com>
2023-09-13 23:38:45 -0500

    updated hash version

--- include/defs.h
+++ include/defs.h
@@ -8,4 +8,4 @@ version is vX.X.X.Julian Date since 2021 Jan 25
                                    -> (short hash for git commit) 
                                              -> (Build Version)
 */
-#define GAME_NAME "Calaga v0.7.4.1961 (2e100ee) (Alpha x64)"
+#define GAME_NAME "Calaga v0.7.4.1961 (9bb13c4) (Alpha x64)"
