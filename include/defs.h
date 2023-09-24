fd057d9765b5a41baaabc48b23956c313b5144f9
nistaux <nistaux@gmail.com>
2023-09-23 23:10:32 -0500

    last update for new beta release

--- include/defs.h
+++ include/defs.h
@@ -8,5 +8,5 @@ version is vX.X.X.Julian Date since 2021 Jan 25
                                    -> (short hash for git commit) 
                                              -> (Build Version)
 */
-#define GAME_VERSION "v0.8.4.230922 (50ab7a8) (Beta x64)"
+#define GAME_VERSION "v0.9.4.230923 (2aba00a) (Beta x64)"
 #define GAME_NAME "Calaga"
